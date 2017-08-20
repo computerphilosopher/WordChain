3c4756397d1747982772a7009d29d90ce0bda1ae
박범수 <erlk0nig@naver.com>
2017-08-20 21:47:23 +0900

    make_candidate, show_candidate 구현

--- game.rb
+++ game.rb
@@ -1,9 +1,9 @@
 ﻿#encoding: utf-8
 
 class Word
-  def initialize(string)
+  def initialize(dic, string)
     
-    @dic = ["창문", "문지기", "기사도"]
+    @dic = dic
     @string = string
     @candidate = Array.new
   end
@@ -12,14 +12,32 @@ class Word
     return @string[-1,1]
   end
 
-  def is_in?
+  def in_dic?
     return @dic.include?(@string)
+  end
+
+  def make_candidate
+    @dic.each do |word|
+      if word[0] == @string[-1,1]
+        @candidate.push(word)
+      end
+    end
+  end
+
+  def show_candidate
+    @candidate.each do |word|
+      p word
+    end
   end
 end
 
-w1 = Word.new("기사도")
 
-p w1.last_letter
+dic = ["창문", "문지기", "기사도"]
 
-puts w1.is_in?
+w1 = Word.new(dic, "유리문")
+
+w1.make_candidate
+
+w1.show_candidate
+
 
