50221bd79c967bacf111bfb3d6d234c8ac07b571
박범수 <erlk0nig@naver.com>
2017-08-21 14:44:22 +0900

    텍스트 파일을 배열로 만드는 기능 구현

--- game.rb
+++ game.rb
@@ -4,40 +4,45 @@ class Word
   def initialize(dic, string)
     
     @dic = dic
-    @string = string
+    @user_word = string
     @candidate = Array.new
   end
 
   def last_letter
-    return @string[-1,1]
+    return @user_word[-1,1]
   end
 
   def in_dic?
-    return @dic.include?(@string)
+    return @dic.include?(user_word)
   end
 
   def make_candidate
     @dic.each do |word|
-      if word[0] == @string[-1,1]
+      if word[0] == @user_word[-1,1]
         @candidate.push(word)
       end
     end
   end
 
   def show_candidate
-    @candidate.each do |word|
+    if @candidate == nil 
+      puts "사전에 없습니다."
+    else
+      @candidate.each do |word|
       p word
+      end
     end
   end
 end
 
 
-dic = ["창문", "문지기", "기사도"]
+test = Array.new
+    
+f = File.open('kor_dic.txt')
+a = f.readlines
+puts a[2]
 
-w1 = Word.new(dic, "유리문")
 
-w1.make_candidate
 
-w1.show_candidate
 
 
