29dcdcce664f0ebf35bf78f0cafad8f3eeeda55d
박범수 <erlk0nig@naver.com>
2017-09-04 22:10:44 +0900

    자모 분리 기초 작업

---
+++ spl.rb
@@ -0,0 +1,30 @@
+﻿#encode
+
+class String
+
+  def initialize(input)
+    @letter = input
+    @unicode = input.unpack("U*")[0]
+  end
+
+  def split 
+    start_point = 44032
+    @chosung = ((@unicode-start_point)/28)/21
+
+    @joongsung = (@unicode-start_point/28)%21
+
+    @jongsung = (@unicode-start_point/28)%28
+    
+    puts @chosung
+    puts @joongsung
+    puts @jongsung
+  end
+
+end
+
+
+
+
+test = String.new('다') 
+test.split
+
